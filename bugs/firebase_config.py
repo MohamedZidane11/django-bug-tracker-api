3c9328dd188b570192d8b31def08066b671f1e39
Mohamed Zidane <mohamedzidane11@gmail.com>
2025-08-03 20:01:58 +0300

    adding urls.py, firebase_config.py

---
+++ bugs/firebase_config.py
@@ -0,0 +1,14 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+from django.conf import settings
+import os
+
+# Initialize Firebase
+if not firebase_admin._apps:
+    cred = credentials.Certificate(settings.FIREBASE_CREDENTIALS_PATH)
+    firebase_admin.initialize_app(cred, {
+        'projectId': settings.FIREBASE_PROJECT_ID,
+    })
+
+# Get Firestore client
+db = firestore.client()
